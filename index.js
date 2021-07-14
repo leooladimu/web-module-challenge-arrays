94872a42cbd703d0e03312846b68a7d0789e33e2
leooladimu <82811737+leooladimu@users.noreply.github.com>
2021-07-14 19:23:13 -0400

    more js assigment

--- index.js
+++ index.js
@@ -81,8 +81,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
- /*your code here*/
+function addFlavor(arr, str){
+  arr.shift(str);
+  return arr;
 }
 
 
@@ -96,9 +97,10 @@ Use the removeLastFlavor function below to do the following:
 
   For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
-
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+// .pop
+function removeLastFlavor(arr){
+  arr.pop(arr[arr.length - 1]);
+  return arr;
 }
 
 
@@ -114,8 +116,8 @@ Use the getFlavorByIndex function below to do the following:
   For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-  /*your code here*/
+function getFlavorByIndex(arr, num){
+  return arr[num];
 }
 
 
@@ -134,10 +136,15 @@ Use the removeFlavorByName function below to do the following:
   HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-  /*your code here*/
+function removeFlavorByName(arr, flav){
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === flav) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
 }
-
+console.log(removeFlavorByName(originalFlavors, 'Rocky Road'));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -160,8 +167,15 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(arr, str){
+  let x = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes(str)) {
+      x.push(arr[i]);
+    }
+    return x;
+  }
+  
 }
 
 
